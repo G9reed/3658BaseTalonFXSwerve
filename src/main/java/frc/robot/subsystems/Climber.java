df09aa2216f518cefee39f915e1925d4f033f25c
github3658 <github@team3658.com>
2024-02-12 12:53:11 -0500

    Clear up issues in the climber code

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Climber extends SubsystemBase {
     private final CANSparkMax liftMotor;
